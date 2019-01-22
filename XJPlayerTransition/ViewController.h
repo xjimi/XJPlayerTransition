001d94bed3bd30a11bd70e97584be721854e83da
XJIMI <fn5128@gmail.com>
2019-01-22 10:55:51 +0800

    Initial Commit

---
+++ XJPlayerTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJPlayerTransition
+//
+//  Created by XJIMI on 2019/1/22.
+//  Copyright © 2019 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XJPlayerTransition/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XJPlayerTransition
+//
+//  Created by XJIMI on 2019/1/22.
+//  Copyright © 2019 XJIMI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
